bd143806bbb0d05f7157070997a97db63e55ea8a
Carlos Augusto <carlosmedeiros.dev@gmail.com>
2024-11-06 15:50:33 -0300

    feat: card-component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { FormComponent } from './FormComponent';
 export { Header } from './Header';
 export * from './BannerImage';
+export * from './CardComponent';
 export * from './Logo';
 export * from './StyledButton';
 export * from './StyledInput';
