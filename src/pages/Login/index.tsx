6ce88cd1a678aa984ba4239a91cf4fe213e8c60e
Carlos Augusto <carlosmedeiros.dev@gmail.com>
2024-11-09 21:16:55 -0300

    feat: login-page-request

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,61 @@
+import { ChangeEvent, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import Cookies from 'js-cookie';
+
+//COMPONENTS
 import { Box, Container, Grid } from "@mui/material";
 import { BannerImage, FormComponent, StyledH1, StyledP, StyledLogo } from "@/components";
-import { pixelToRem } from "@/utils";
+
+//HOOKS
+import { useFormValid, usePost } from "@/hooks";
+
+//UTILS
+import { pixelToRem, jwtExpirationDateConverter } from "@/utils";
+
+//TYPES
+import { MessageProps, LoginData, LoginPostData, DecodedJWT } from '@/types';
 
 
 export function Login() {
+
+    const navigate = useNavigate()
+
+    const inputs = [
+        { type: 'email', placeholder: 'Email' },
+        { type: 'password', placeholder: 'Senha' }
+    ]
+
+    const { data, loading, error, postData } = usePost<LoginData, LoginPostData>('login')
+
+    const { formValues, formValid, handleChange } = useFormValid(inputs)
+
+    const handleMessage = (): MessageProps => {
+        if (!error) return { msg: '', type: 'success' }
+        switch (error) {
+            case 401:
+                return { msg: 'Email ou senha inválidos', type: 'error' }
+            default:
+                return { msg: 'Ocorreu um erro ao fazer login', type: 'error' }
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await postData({ email: String(formValues[0]), password: String(formValues[1]) })
+    }
+
+    useEffect(() => {
+        if (data?.jwt_token) {
+            const decoded: DecodedJWT = jwtDecode(data?.jwt_token)
+            Cookies.set('Autorization', data.jwt_token, {
+                expires: jwtExpirationDateConverter(decoded.exp),
+                secure: true
+            })
+        }
+        if (Cookies.get('Autorization')) navigate('/home')
+    }, [data, navigate])
+
     return (
         <>
             <Box>
@@ -18,14 +70,22 @@ export function Login() {
                                 <StyledP>Faça login para acessar sua conta</StyledP>
                             </Box>
                             <FormComponent
-                                inputs={[
-                                    { type: 'email', placeholder: 'Email' },
-                                    { type: 'password', placeholder: 'Senha' }
-                                ]}
+                                inputs={inputs.map((input, index) => ({
+                                    type: input.type,
+                                    placeholder: input.placeholder,
+                                    value: formValues[index] || '',
+                                    onChange: (e: ChangeEvent<HTMLInputElement>) => handleChange(index, (e.target as HTMLInputElement).value)
+                                }))}
                                 buttons={[
-                                    { className: 'primary', type: 'submit', children: 'Entrar' }
+                                    {
+                                        className: 'primary',
+                                        type: 'submit',
+                                        children: loading ? 'Aguarde...' : 'Entrar',
+                                        disabled: !formValid || loading,
+                                        onClick: handleSubmit
+                                    }
                                 ]}
-                                messages={{ msg: 'Sucesso!!', type: 'success' }}
+                                messages={handleMessage()}
                             />
                         </Container>
                     </Grid>
---
+++ src/utils/jwtExpirationDateConverter.ts
@@ -0,0 +1,13 @@
+/**
+ * Converts JWT exp in days
+ * @param exp - Number to be converted
+ * @returns Converted exp in days
+ */
+
+export function jwtExpirationDateConverter(exp: number): number {
+    const currentTime = Math.floor(Date.now() / 1000); // Convert to seconds
+    const secondsUntilExpiration = exp - currentTime;
+    const secondsInADay = 60 * 60 * 24; // 1 day in seconds
+    const daysUntilExpiration = secondsUntilExpiration / secondsInADay
+    return daysUntilExpiration
+}
