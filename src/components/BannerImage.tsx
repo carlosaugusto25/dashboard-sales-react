decfbef5034ed846bab0f39dd88fa8d7ce7f9669
Carlos Augusto <carlosmedeiros.dev@gmail.com>
2024-11-11 17:23:21 -0300

    feat: adding-unit-tests

--- src/components/BannerImage.tsx
+++ src/components/BannerImage.tsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-import loginImg from "../assets/banner-login.jpg";
 
 export const BannerImage = styled.div`
-    background-image: url(${loginImg});
+    background-image: url(/banner-login.jpg);
     background-repeat: no-repeat;
     background-size: cover;
     height: 100vh;
